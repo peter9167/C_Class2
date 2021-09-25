67d085a92d29eff9f8ed5192c9e5eea2cde3395a
peter <peter9167@naver.com>
2021-09-26 01:58:10 +0900

    first commit

---
+++ C_Class2/main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 12;
+	int num2 = 12;
+	printf("num1 : %d \n", num1);
+	printf("num1++ : %d \n", num1++);
+	printf("num1 : %d \n", num1);
+	printf("++num1 : %d \n", ++num1);
+	return 0;
+}
+
+/*
+int num;
+num = 20;
+printf("%d \n", num);
+*/
+
+/*
+int num1, num2;
+int num3 = 30, num4 = 40;
+
+num1 = num2 = 0;
+
+printf("num1: %d, num2: %d \n", num1, num2);
+printf("num3: %d, num4: %d \n", num3, num4);
+*/
+
+/*
+int num1 = 3, num2 = 4;
+int result1 = num1 + num2;
+int result2 = num1 - num2;
+int result3 = num1 * num2;
+int result4 = num1 / num2;
+int result5 = num1 % num2;
+//num1 = num2 = 0;
+
+printf("num1: %d, num2: %d \n", num1, num2);
+printf("%d + %d = %d \n", num1, num2, result1);
+printf("%d - %d = %d \n", num1, num2, result2);
+printf("%d * %d = %d \n", num1, num2, result3);
+printf("%d / %d = %d(¸ò) \n", num1, num2, result4);
+printf("%d / %d = %d(³ª¸ÓÁö) \n", num1, num2, result5);
+*/
